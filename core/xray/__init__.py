96111813b384623ffaaa1f9824209ad370cf146d
tskon-kz <tsekhmeister.k+kz@gmail.com>
2026-06-23 18:50:25 +0300

    xray refactoring

--- core/xray/__init__.py
+++ core/xray/__init__.py
@@ -1,4 +1,2 @@
 from core.xray._config import write_xray_config
-from core.xray._process import XrayProcess, XrayProcessPool
-
-__all__ = ["XrayProcessPool", "XrayProcess", "write_xray_config"]
+from core.xray._process import XrayProcessPool
